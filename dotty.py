48fadfa5b1a7c8d7390a0e697a8ac7ea287dc6cb
id101010 <aaron@duckpond.ch>
2015-10-14 17:37:26 +0200

    Implemented option parsing and data object

--- dotty.py
+++ dotty.py
@@ -1,27 +1,118 @@
 #!/usr/bin/python2
+#-*- coding: utf-8 -*-
 
-DOTFILELIST = 'dotfiledb'
-
-filelist = open(DOTFILELIST, 'rw')
-data = []
+from optparse import OptionParser
 
 class DBEntry(object):
     ''' Object to store the db content '''
-    TIMESTAMP = ''
     LINKNAME = ''
     SRC = ''
     DST = ''
 
+    # Get/Set functions
+    def setName(self, name):
+        self.LINKNAME = name
+    def getName(self):
+        return self.LINKNAME
+    def setSRC(self, src):
+        self.SRC = src
+    def getSRC(self):
+        return self.SRC
+    def setDST(self, dst):
+        self.DST = dst
+    def getDST(self):
+        return self.DST
+
+    # fill the data object
+    def fill(self, src, dst, name):
+        self.setName(name)
+        self.setSRC(src)
+        self.setDST(dst)
+
 class DotfileDBTools(object):
     ''' Tools used to populate the database '''
 
-    def parseList(self, filename):
-        # Parse filelist and generate an internal list.
-        for i, line in enumerate(filelist):
+    i = 0;
+
+    # Parses the dotfiledb and generates a DBEntry object
+    def getDotfiles(self, filename):
+        dotfiles = []
+        with open(filename, "r") as dblist:
+            lines = [line.strip() for line in dblist if line.strip()]
+        for line in lines:
             if not line.startswith('#'):
-                fl.append(line)
+                src, dst, name = line.split(":")
+                dotfiles.append(DBEntry())
+                dotfiles[-1].fill(src,dst,name)
+                print dotfiles[-1].getName
+                print dotfiles[-1].getSRC
+                print dotfiles[-1].getDST
 
-    def appendObject()
+    # Appends a dotfile to the dotfiledb
+    def linkObject(self, DBEntry):
+        #TODO
+        print "not implemented"
 
-class Dotter(object):
-    ''' Control logic '''
+    # Appends a dotfile to the dotfiledb
+    def listObject(self, DBEntry):
+        #TODO
+        print "not implemented"
+
+if __name__ == "__main__":
+    '''Control logic, if not loaded as a module'''
+    # Variables
+    dbfilename = './dotfiledb'
+    
+    # Option parsing
+    parser = OptionParser()
+    
+
+    parser.add_option(  "-d", 
+                        "--dbfile",
+                        action="store", 
+                        type="string", 
+                        dest="dbfilename",
+                        help="Path for the DB file.")
+
+    parser.add_option(  "-a", 
+                        "--adddotfile", 
+                        action="store_true", 
+                        dest="add",
+                        help="Adds a dotfile to the DB.")
+
+    parser.add_option(  "-t", 
+                        "--testdb", 
+                        action="store_true",
+                        dest="test",
+                        help="List entrys in the DB.")
+    
+    parser.add_option(  "-l",
+                        "--link",
+                        action="store_true",
+                        dest="link",
+                        help="Create links according to the DB.")
+
+    (options, args) = parser.parse_args()
+
+    if options.add is True:
+        print "add not implemented"
+
+    if options.test is True:
+        tools = DotfileDBTools()
+        tools.getDotfiles(options.dbfilename)
+
+
+    if options.link is True:
+        print "link not implemented"
+
+
+
+    test = DBEntry()
+    test.setName("test")
+    test.setSRC("adfjasdlfk")
+    test.setDST("asdasd")
+
+    dblist = []
+    dblist.append(test)
+
+    print test.getName
